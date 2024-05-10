e75b449b888d069a50120f7474fdfa71ff22acd4
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-05-11 01:50:16 +0300

    hero class created

---
+++ hero.h
@@ -0,0 +1,39 @@
+#ifndef HERO_H
+#define HERO_H
+
+#include <QString>
+#include <QFileDialog>
+
+struct Ability {
+    QString name;
+    QDir image;
+    int manaCost;
+};
+
+class Hero {
+private:
+    QString name;
+    QDir image;
+
+    int health;
+    int mana;
+
+    Ability* abilities = new Ability[3];
+
+public:
+    Hero(QString name, QDir image, int health, int mana, Ability abilities[3]);
+
+    QString getName();
+    QDir getImage();
+    int getHealth();
+    int getMana();
+    Ability* getAbilities();
+
+    void castSkill();
+    void getDamage(int);
+    void useItem();
+};
+
+
+
+#endif // HERO_H
